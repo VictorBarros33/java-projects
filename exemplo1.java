e19f39d2dcebb662c70a3b040714f5f7f48e1037
Víctor Aurélio <victoraurelio42@gmail.com>
2025-11-11 14:04:54 -0300

    Add files via upload

---
+++ exemplo1.java
@@ -0,0 +1,15 @@
+package exemplo1;
+
+public class exemplo1 {
+
+	public static void main(String[] args) {
+		int x, y;
+		
+		x = 5;
+		y = 2*5;
+		
+		System.out.println(y);
+
+	}
+
+}
---
+++ exemplo2.java
@@ -0,0 +1,18 @@
+package exemplo2;
+
+public class exemplo2 {
+
+	public static void main(String[] args) {
+		
+		int x;
+		double y;
+		
+		x = 5;
+		
+		y = 2*x;
+		
+		System.out.println(y);
+		System.out.println(x);
+	}
+
+}
