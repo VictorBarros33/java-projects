fc7843aa2c3e118d07cab4f59785d6a5fb503f87
Víctor Aurélio <victoraurelio42@gmail.com>
2025-11-14 23:02:20 -0300

    Add files via upload

---
+++ switchCase.java
@@ -0,0 +1,47 @@
+package exemplo6;
+
+import java.util.Scanner;
+
+public class switchCase {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Digite um numero inteiro de 1 a 7, que represente o dia da semana sendo 1 igual a Domingo");
+
+		int x = sc.nextInt();
+		String dia;
+		
+		switch (x) {
+		case 1: 
+			dia = "Domingo";
+			break; 
+		case 2: 
+			dia = "Segunda";
+			break; 
+		case 3: 
+			dia = "Terça";
+			break; 
+		case 4: 
+			dia = "Quarta";
+			break; 
+		case 5: 
+			dia = "Quinta";
+			break; 
+		case 6: 
+			dia = "Sexta";
+			break; 
+		case 7: 
+			dia = "Sabado";
+			break; 
+		default : 
+			dia = "Valor indefinido";
+			break; 
+		}
+
+		System.out.println("Dia da semana = " + dia);
+			sc.close();
+	}
+
+}
