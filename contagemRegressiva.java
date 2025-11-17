662177bad3bcd96cbc95eb1a58538120f969b8c9
Víctor Aurélio <victoraurelio42@gmail.com>
2025-11-16 22:53:38 -0300

    Add files via upload

---
+++ contagemRegressiva.java
@@ -0,0 +1,13 @@
+package exemplo6;
+
+public class contagemRegressiva {
+
+	public static void main(String[] args) {
+		
+		for (int i=4;i>=0;i--) {
+			System.out.println("Valor de i: " + i);
+		}
+
+	}
+
+}
